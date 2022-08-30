9294ed2beea224999b26b8982078e71f3e1cc6ad
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-08-30 10:11:50 -0300

    Criacao da classe endereco, testada

---
+++ Banco_Morangao/Banco_Morangao/Endereco.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_Morangao
+{
+    internal class Endereco
+    {
+        private String Logradouro { get; set; }
+        private String Numero { get; set; }
+        private String Bairro { get; set; }
+        private String Cidade { get; set; }
+        private String Cep { get; set; }
+        private String Complemento { get; set; }
+
+        //construtor vazio
+        public Endereco()
+        {
+        }
+
+        //construtor de endereco sem complemento
+        public Endereco(string logradouro, string numero, string bairro, string cidade, string cep)
+        {
+            Logradouro = Console.ReadLine();
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Cep = cep;
+        }
+
+        //construtor de endereco com complemento
+        public Endereco(string logradouro, string numero, string bairro, string cidade, string cep, string complemento)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Cep = cep;
+            Complemento = complemento;
+        }
+
+        //metodo override toString para escrita dos dados
+        public override string ToString()
+        {
+            return $"Endereço: {Logradouro}, n°: {Numero}, Bairro: {Bairro}, Cidade: {Cidade}, CEP: {Cep}, Complemento: {Complemento}".ToString();
+        }
+
+
+    }
+}
--- Banco_Morangao/Banco_Morangao/Program.cs
+++ Banco_Morangao/Banco_Morangao/Program.cs
@@ -6,8 +6,40 @@ namespace Banco_Morangao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            Endereco endereco = coletarEndereco();
+            Console.WriteLine("Endereço informado: ");
+            Console.WriteLine(endereco);
+
         }
+
+        //metodo para coletar endereco
+        static Endereco coletarEndereco()
+        {
+            string logradouro, bairro, cidade, cep, complemento;
+            int numero;
+
+            Console.WriteLine("Informe o nome da rua: ");
+            logradouro = Console.ReadLine();
+
+            Console.WriteLine("Informe o numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o bairro: ");
+            bairro = Console.ReadLine();
+
+            Console.WriteLine("Informe a cidade: ");
+            cidade = Console.ReadLine();
+
+            Console.WriteLine("Informe o CEP: ");
+            cep = Console.ReadLine();
+
+            Console.WriteLine("Informe o complemento: ");
+            complemento = Console.ReadLine();
+
+            return new Endereco(logradouro, numero.ToString(), bairro, cidade, cep, complemento);
+        }
+
+
+
     }
 }
