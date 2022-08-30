17973e20bf8c9dd28c06d9c05cad187203d8566e
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-08-30 10:57:04 -0300

    Criado construtores da classe pessoa

--- Banco_Morangao/Banco_Morangao/Agencia.cs
+++ Banco_Morangao/Banco_Morangao/Agencia.cs
@@ -8,7 +8,7 @@ namespace Banco_Morangao
 {
     internal class Agencia
     {
-        private String NumAgencia { get; set; }
+        private String Ag { get; set; }
         //adicionar a lista de clientes após criar a classe clientes
         //adicionar a lista de funcionarios após criar a classe funcionários
 
--- Banco_Morangao/Banco_Morangao/Pessoa.cs
+++ Banco_Morangao/Banco_Morangao/Pessoa.cs
@@ -8,5 +8,27 @@ namespace Banco_Morangao
 {
     internal class Pessoa
     {
+        private String Nome { get; set; }
+        private String Telefone { get; set; }
+
+        private Endereco endereco;
+        private String Email { get; set; }
+        private String Cpf { get; set; }
+        private String Genero { get; set; }
+
+        //metodo construtor vazio
+        public Pessoa() { }
+
+        //metodo construtor com parametros
+        public Pessoa(string nome, string telefone, Endereco endereco, string email, string cpf, string genero)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            this.endereco = endereco;
+            Email = email;
+            Cpf = cpf;
+            Genero = genero;
+        }
+
     }
 }
