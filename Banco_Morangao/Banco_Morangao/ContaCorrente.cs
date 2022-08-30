2c040670f0e5bade13f53cbe08d67f722a80d27a
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-08-30 13:51:57 -0300

    criado classe conta corrente

---
+++ Banco_Morangao/Banco_Morangao/ContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_Morangao
+{
+    internal class ContaCorrente
+    {
+        private bool _habilitada;
+        private Cliente _cliente;
+        private Agencia _agencia;
+        //private String _senha;
+        private String NumConta;
+        private double _saldo;
+        private double _limite;
+        //private Cartao cartao;
+        private String TipoConta;
+    }
+}
